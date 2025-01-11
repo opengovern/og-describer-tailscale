2f5a3bec4ffed20ae613df71224707aa89adabb1
unknown <arshia.bahar@aut.ac.ir>
2025-01-11 15:06:22 +0330

    feat: added device describer

---
+++ provider/describer/devices.go
@@ -0,0 +1,233 @@
+package describer
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/opengovern/og-describer-tailscale/pkg/sdk/models"
+	"github.com/opengovern/og-describer-tailscale/provider/model"
+	resilientbridge "github.com/opengovern/resilient-bridge"
+	"net/url"
+	"sync"
+)
+
+func ListDevices(ctx context.Context, handler *resilientbridge.ResilientBridge, stream *models.StreamSender) ([]models.Resource, error) {
+	var wg sync.WaitGroup
+	tailScaleChan := make(chan models.Resource)
+	errorChan := make(chan error, 1) // Buffered channel to capture errors
+
+	go func() {
+		defer close(tailScaleChan)
+		defer close(errorChan)
+		if err := processDevices(ctx, handler, tailScaleChan, &wg); err != nil {
+			errorChan <- err // Send error to the error channel
+		}
+		wg.Wait()
+	}()
+
+	var values []models.Resource
+	for {
+		select {
+		case value, ok := <-tailScaleChan:
+			if !ok {
+				return values, nil
+			}
+			if stream != nil {
+				if err := (*stream)(value); err != nil {
+					return nil, err
+				}
+			} else {
+				values = append(values, value)
+			}
+		case err := <-errorChan:
+			return nil, err
+		}
+	}
+}
+
+func GetDevice(ctx context.Context, handler *resilientbridge.ResilientBridge, resourceID string) (*models.Resource, error) {
+	device, err := processDevice(ctx, handler, resourceID)
+	if err != nil {
+		return nil, err
+	}
+	latencyMap := make(map[string]model.Latency)
+	for key, value := range device.ClientConnectivity.Latency {
+		latency := model.Latency{
+			Preferred: value.Preferred,
+			LatencyMs: value.LatencyMs,
+		}
+		latencyMap[key] = latency
+	}
+	clientSupports := model.ClientCapabilities{
+		HairPinning: device.ClientConnectivity.ClientSupports.HairPinning,
+		IPv6:        device.ClientConnectivity.ClientSupports.IPv6,
+		PCP:         device.ClientConnectivity.ClientSupports.PCP,
+		PMP:         device.ClientConnectivity.ClientSupports.PMP,
+		UDP:         device.ClientConnectivity.ClientSupports.UDP,
+		UPnP:        device.ClientConnectivity.ClientSupports.UPnP,
+	}
+	clientConnectivity := model.ClientConnectivity{
+		Endpoints:             device.ClientConnectivity.Endpoints,
+		MappingVariesByDestIP: device.ClientConnectivity.MappingVariesByDestIP,
+		Latency:               latencyMap,
+		ClientSupports:        &clientSupports,
+	}
+	postureIdentity := model.PostureIdentity{
+		SerialNumbers: device.PostureIdentity.SerialNumbers,
+		Disabled:      device.PostureIdentity.Disabled,
+	}
+	value := models.Resource{
+		ID:   device.ID,
+		Name: device.Name,
+		Description: model.DeviceDescription{
+			Addresses:                 device.Addresses,
+			ID:                        device.ID,
+			NodeID:                    device.NodeID,
+			User:                      device.User,
+			Name:                      device.Name,
+			Hostname:                  device.Hostname,
+			ClientVersion:             device.ClientVersion,
+			UpdateAvailable:           device.UpdateAvailable,
+			OS:                        device.OS,
+			Created:                   device.Created,
+			LastSeen:                  device.LastSeen,
+			KeyExpiryDisabled:         device.KeyExpiryDisabled,
+			Expires:                   device.Expires,
+			Authorized:                device.Authorized,
+			IsExternal:                device.IsExternal,
+			MachineKey:                device.MachineKey,
+			NodeKey:                   device.NodeKey,
+			BlocksIncomingConnections: device.BlocksIncomingConnections,
+			EnabledRoutes:             device.EnabledRoutes,
+			AdvertisedRoutes:          device.AdvertisedRoutes,
+			ClientConnectivity:        &clientConnectivity,
+			Tags:                      device.Tags,
+			TailnetLockError:          device.TailnetLockError,
+			TailnetLockKey:            device.TailnetLockKey,
+			PostureIdentity:           &postureIdentity,
+		},
+	}
+	return &value, nil
+}
+
+func processDevices(ctx context.Context, handler *resilientbridge.ResilientBridge, tailScaleChan chan<- models.Resource, wg *sync.WaitGroup) error {
+	var deviceListResponse model.DeviceListResponse
+	baseURL := "/v2/tailnet/-/devices"
+
+	params := url.Values{}
+	params.Set("fields", "all")
+	finalURL := fmt.Sprintf("%s?%s", baseURL, params.Encode())
+
+	req := &resilientbridge.NormalizedRequest{
+		Method:   "GET",
+		Endpoint: finalURL,
+		Headers:  map[string]string{"accept": "application/json"},
+	}
+
+	resp, err := handler.Request("tailsclae", req)
+	if err != nil {
+		return fmt.Errorf("request execution failed: %w", err)
+	}
+
+	if resp.StatusCode >= 400 {
+		return fmt.Errorf("error %d: %s", resp.StatusCode, string(resp.Data))
+	}
+
+	if err = json.Unmarshal(resp.Data, &deviceListResponse); err != nil {
+		return fmt.Errorf("error parsing response: %w", err)
+	}
+
+	for _, device := range deviceListResponse.Devices {
+		wg.Add(1)
+		go func(device model.DeviceJSON) {
+			defer wg.Done()
+			latencyMap := make(map[string]model.Latency)
+			for key, value := range device.ClientConnectivity.Latency {
+				latency := model.Latency{
+					Preferred: value.Preferred,
+					LatencyMs: value.LatencyMs,
+				}
+				latencyMap[key] = latency
+			}
+			clientSupports := model.ClientCapabilities{
+				HairPinning: device.ClientConnectivity.ClientSupports.HairPinning,
+				IPv6:        device.ClientConnectivity.ClientSupports.IPv6,
+				PCP:         device.ClientConnectivity.ClientSupports.PCP,
+				PMP:         device.ClientConnectivity.ClientSupports.PMP,
+				UDP:         device.ClientConnectivity.ClientSupports.UDP,
+				UPnP:        device.ClientConnectivity.ClientSupports.UPnP,
+			}
+			clientConnectivity := model.ClientConnectivity{
+				Endpoints:             device.ClientConnectivity.Endpoints,
+				MappingVariesByDestIP: device.ClientConnectivity.MappingVariesByDestIP,
+				Latency:               latencyMap,
+				ClientSupports:        &clientSupports,
+			}
+			postureIdentity := model.PostureIdentity{
+				SerialNumbers: device.PostureIdentity.SerialNumbers,
+				Disabled:      device.PostureIdentity.Disabled,
+			}
+			value := models.Resource{
+				ID:   device.ID,
+				Name: device.Name,
+				Description: model.DeviceDescription{
+					Addresses:                 device.Addresses,
+					ID:                        device.ID,
+					NodeID:                    device.NodeID,
+					User:                      device.User,
+					Name:                      device.Name,
+					Hostname:                  device.Hostname,
+					ClientVersion:             device.ClientVersion,
+					UpdateAvailable:           device.UpdateAvailable,
+					OS:                        device.OS,
+					Created:                   device.Created,
+					LastSeen:                  device.LastSeen,
+					KeyExpiryDisabled:         device.KeyExpiryDisabled,
+					Expires:                   device.Expires,
+					Authorized:                device.Authorized,
+					IsExternal:                device.IsExternal,
+					MachineKey:                device.MachineKey,
+					NodeKey:                   device.NodeKey,
+					BlocksIncomingConnections: device.BlocksIncomingConnections,
+					EnabledRoutes:             device.EnabledRoutes,
+					AdvertisedRoutes:          device.AdvertisedRoutes,
+					ClientConnectivity:        &clientConnectivity,
+					Tags:                      device.Tags,
+					TailnetLockError:          device.TailnetLockError,
+					TailnetLockKey:            device.TailnetLockKey,
+					PostureIdentity:           &postureIdentity,
+				},
+			}
+			tailScaleChan <- value
+		}(device)
+	}
+	return nil
+}
+
+func processDevice(ctx context.Context, handler *resilientbridge.ResilientBridge, resourceID string) (*model.DeviceJSON, error) {
+	var device model.DeviceJSON
+	baseURL := "/v2/device/"
+
+	finalURL := fmt.Sprintf("%s%s", baseURL, resourceID)
+
+	req := &resilientbridge.NormalizedRequest{
+		Method:   "GET",
+		Endpoint: finalURL,
+		Headers:  map[string]string{"accept": "application/json"},
+	}
+
+	resp, err := handler.Request("tailscale", req)
+	if err != nil {
+		return nil, fmt.Errorf("request execution failed: %w", err)
+	}
+
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("error %d: %s", resp.StatusCode, string(resp.Data))
+	}
+
+	if err = json.Unmarshal(resp.Data, &device); err != nil {
+		return nil, fmt.Errorf("error parsing response: %w", err)
+	}
+
+	return &device, nil
+}
--- provider/model/model.go
+++ provider/model/model.go
@@ -4,4 +4,118 @@
 
 package model
 
+import "time"
+
 type Metadata struct{}
+
+type ClientConnectivityJSON struct {
+	Endpoints             []string                `json:"endpoints"`
+	MappingVariesByDestIP bool                    `json:"mappingVariesByDestIP"`
+	Latency               map[string]LatencyJSON  `json:"latency"`
+	ClientSupports        *ClientCapabilitiesJSON `json:"clientSupports"`
+}
+
+type ClientConnectivity struct {
+	Endpoints             []string
+	MappingVariesByDestIP bool
+	Latency               map[string]Latency
+	ClientSupports        *ClientCapabilities
+}
+
+type LatencyJSON struct {
+	Preferred bool    `json:"preferred"`
+	LatencyMs float64 `json:"latencyMs"`
+}
+
+type Latency struct {
+	Preferred bool
+	LatencyMs float64
+}
+
+type ClientCapabilitiesJSON struct {
+	HairPinning bool `json:"hairPinning"`
+	IPv6        bool `json:"ipv6"`
+	PCP         bool `json:"pcp"`
+	PMP         bool `json:"pmp"`
+	UDP         bool `json:"udp"`
+	UPnP        bool `json:"upnp"`
+}
+
+type ClientCapabilities struct {
+	HairPinning bool
+	IPv6        bool
+	PCP         bool
+	PMP         bool
+	UDP         bool
+	UPnP        bool
+}
+
+type PostureIdentityJSON struct {
+	SerialNumbers []string `json:"serialNumbers"`
+	Disabled      bool     `json:"disabled"`
+}
+
+type PostureIdentity struct {
+	SerialNumbers []string
+	Disabled      bool
+}
+
+type DeviceListResponse struct {
+	Devices []DeviceJSON `json:"devices"`
+}
+
+type DeviceJSON struct {
+	Addresses                 []string                `json:"addresses"`
+	ID                        string                  `json:"id"`
+	NodeID                    string                  `json:"nodeId"`
+	User                      string                  `json:"user"`
+	Name                      string                  `json:"name"`
+	Hostname                  string                  `json:"hostname"`
+	ClientVersion             string                  `json:"clientVersion"`
+	UpdateAvailable           bool                    `json:"updateAvailable"`
+	OS                        string                  `json:"os"`
+	Created                   *time.Time              `json:"created"`
+	LastSeen                  *time.Time              `json:"lastSeen"`
+	KeyExpiryDisabled         bool                    `json:"keyExpiryDisabled"`
+	Expires                   *time.Time              `json:"expires"`
+	Authorized                bool                    `json:"authorized"`
+	IsExternal                bool                    `json:"isExternal"`
+	MachineKey                string                  `json:"machineKey"`
+	NodeKey                   string                  `json:"nodeKey"`
+	BlocksIncomingConnections bool                    `json:"blocksIncomingConnections"`
+	EnabledRoutes             []string                `json:"enabledRoutes"`
+	AdvertisedRoutes          []string                `json:"advertisedRoutes"`
+	ClientConnectivity        *ClientConnectivityJSON `json:"clientConnectivity"`
+	Tags                      []string                `json:"tags"`
+	TailnetLockError          string                  `json:"tailnetLockError"`
+	TailnetLockKey            string                  `json:"tailnetLockKey"`
+	PostureIdentity           *PostureIdentityJSON    `json:"postureIdentity"`
+}
+
+type DeviceDescription struct {
+	Addresses                 []string
+	ID                        string
+	NodeID                    string
+	User                      string
+	Name                      string
+	Hostname                  string
+	ClientVersion             string
+	UpdateAvailable           bool
+	OS                        string
+	Created                   *time.Time
+	LastSeen                  *time.Time
+	KeyExpiryDisabled         bool
+	Expires                   *time.Time
+	Authorized                bool
+	IsExternal                bool
+	MachineKey                string
+	NodeKey                   string
+	BlocksIncomingConnections bool
+	EnabledRoutes             []string
+	AdvertisedRoutes          []string
+	ClientConnectivity        *ClientConnectivity
+	Tags                      []string
+	TailnetLockError          string
+	TailnetLockKey            string
+	PostureIdentity           *PostureIdentity
+}
